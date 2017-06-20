a5c6b23a7746f213dd41bb7f0dd11c11fecf31c5
Ziaul Hasan <ziaul.cuet02@gmail.com>
2017-06-20 22:37:42 +0600

    Removed unnecessary controller actions

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,19 +2,6 @@ var numeral = require('numeral');
 var bcrypt = require('bcrypt-nodejs');
 var dateFormat = require('dateformat');
 
-exports.loggedIn = function(req, res, next)
-{
-	if (req.session.user) { // req.session.passport._id
-
-		next();
-
-	} else {
-		//res.redirect('/login');
-		next();
-
-	}
-
-}
 
 exports.home = function(req, res) {
 	res.render('pages/home', {
@@ -23,41 +10,4 @@ exports.home = function(req, res) {
 		session: req.session,
 	
 	 });
-}
-
-
-exports.signup = function(req, res) {
-	if (req.session.user) {
-
-		res.redirect('/home');
-
-	} else {
-		res.render('users/signup', {
-			error : req.flash("error"),
-			success: req.flash("success"),
-			session:req.session
-		});
-	}
-
-}
-
-
-exports.login = function(req, res) {
-
-
-	
-	if (req.session.user) {
-
-		res.redirect('/home');
-
-	} else {
-
-		res.render('users/login', {
-			error : req.flash("error"),
-			success: req.flash("success"),
-			session:req.session
-		});
-
-	}
-	
 }
